a291be5664a34efa75181cea6283999a6b618d22
avkadu <32651567+avkadu@users.noreply.github.com>
2019-01-03 19:58:11 +0530

    updated

--- CustomerInquiry.sql
+++ CustomerInquiry.sql
@@ -5,13 +5,13 @@ CREATE TABLE [dbo].[Customer](
 	[CustomerID] [int]  NOT NULL Identity PRIMARY KEY,
 	[CustomerName] [varchar](30) NULL,
 	[ContactEmail] [varchar] (25)NULL,
-	[MobileNo] [int] NULL
+	[MobileNo] varchar (10) NULL
 )
 
 CREATE TABLE [dbo].[Transaction](
 	[TransactionID] [int]  NOT NULL Identity PRIMARY KEY,
 	[TransactionDateTime] DateTime NULL,
-	[Amount] decimal (18,2) NULL,
+	[Amount] decimal(18,2) NULL,
 	[CurrencyCode] [varchar] (3) null,
 	[Status] varchar(10) null,
 	[CustomerID] [int]  
